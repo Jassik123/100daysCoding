838bd691a62e08a263cbf2747cf076a19ed5fc28
Jaspreet Kaur <kk.jas2003@gmail.com>
2024-03-03 22:37:27 +0530

    Day12

---
+++ Day12/majorityelement1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n=nums.length;
+       int count=0;
+       int ele=0;
+       for(int i=0;i<n;i++){
+           if(count==0){
+               count=1;
+               ele=nums[i];
+           }else if(nums[i]==ele){
+               count++;
+           }
+           else{
+               count--;
+           }
+       }
+       count=0;
+       for(int i=0;i<n;i++){
+           if(nums[i]==ele) count++;
+
+           if(count>(n/2)) return ele;
+
+       } 
+       return -1;
+        
+        
+}}
+
