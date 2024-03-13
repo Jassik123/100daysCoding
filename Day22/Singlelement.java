d2b19b137f4c66949ba196ce87980aa0ab6ec074
Jaspreet Kaur <kk.jas2003@gmail.com>
2024-03-13 12:33:17 +0530

    day22

---
+++ Day22/Singlelement.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+    int n=nums.length;
+    int start=0;
+    int end=n-1;
+    while(start<end){
+        int mid =start+(end-start)/2;
+if(mid%2==1){
+    mid--;
+}
+if(nums[mid]!=nums[mid+1]){
+    end=mid;
+}else{
+    start=mid+2;
+}
+
+    }
+    return nums[start];
+    }
+}
